86c9ecc24eee5bc8cef5ce0966987d208a6fbbad
Кирилл Протасов <kirill_protasow228@mail.ru>
2024-02-02 19:09:55 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+from library import pygame
+
+
+pygame.display.set_caption('Большая задача по Maps API.')
+screen = pygame.display.set_mode((600, 450))
+
+fps = 30
+clock = pygame.time.Clock()
+name_map = 'map.png'
